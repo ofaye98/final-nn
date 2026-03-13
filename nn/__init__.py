795010d21c174ba08096ad6a31ab282f4e5f13f6
Faye Orcales <75501045+ofaye98@users.noreply.github.com>
2026-03-13 00:28:10 -0700

    edit to make pip install

--- nn/__init__.py
+++ nn/__init__.py
@@ -3,4 +3,16 @@ BMI203: Biocomputing Algorithms - Winter 2023
 Final project: neural networks
 """
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
+
+from .nn import NeuralNetwork
+from .preprocess import sample_seqs, one_hot_encode_seqs
+from .io import read_text_file, read_fasta_file
+
+__all__ = [
+    "NeuralNetwork",
+    "sample_seqs",
+    "one_hot_encode_seqs",
+    "read_text_file",
+    "read_fasta_file"
+]
